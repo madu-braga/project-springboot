37aeda8fe7e7975086ca8ea6e9fa4b50933a48a1
Duda Braga <f.macedo.braga@gmail.com>
2023-09-30 22:29:38 +0000

    ddl

--- DDL.sql
+++ DDL.sql
@@ -1,52 +1,65 @@
-drop schema if exists spring;
+-- Drop the schema and user if they exist
+DROP SCHEMA IF EXISTS spring;
+DROP USER IF EXISTS 'user'@'localhost';
 
-drop user if exists 'user'@'localhost';
+-- Create the schema and user
+CREATE SCHEMA spring;
+CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass123';
 
-create schema spring;
+-- Grant privileges to the user
+GRANT SELECT, INSERT, DELETE, UPDATE ON spring.* TO 'user'@'localhost';
 
-create user 'user'@'localhost' identified by 'pass123';
+-- Use the 'spring' schema
+USE spring;
 
-grant select, insert, delete, update on spring.* to user@'localhost';
-
-use spring;
-
-Create table usr_usuario(
-    usr_id bigint unsigned not null auto_increment,
-    usr_nome varchar(20) not null,
-    usr_senha varchar(150) not null,
-    primary key (usr_id),
-    unique key uni_usuario_nome (usr_nome)
+-- Create the 'usr_usuario' table
+CREATE TABLE usr_usuario (
+    usr_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    usr_nome VARCHAR(20) NOT NULL,
+    usr_senha VARCHAR(150) NOT NULL,
+    PRIMARY KEY (usr_id),
+    UNIQUE KEY uni_usuario_nome (usr_nome)
 );
 
-Create table aut_autorizacao(
-    aut_id bigint unsigned not null,
-    aut_nome varchar(20) not null,
-    primary key (aut_id),
-    unique key uni_aut_nome (aut_nome)
-)
-
-Create table uau_usuario_autorizacao(
-    usr_id bigint unsigned not null,
-    aut_id bigint unsigned not null,
-    primary key (usr_id, aut_id),
-    foreign key aut_usuario_fk (usr_id) referencer usr_usuario (usr_id) on delete restrict on update cascade,
-    foreign key aut_autorizacao_fk (aut_id) references aut_autorizacao (aut_id) on delete restrict on update cascade
+-- Create the 'aut_autorizacao' table
+CREATE TABLE aut_autorizacao (
+    aut_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    aut_nome VARCHAR(20) NOT NULL,
+    PRIMARY KEY (aut_id),
+    UNIQUE KEY uni_aut_nome (aut_nome)
 );
 
-Create table ant_anotacao(
-    ant_id bigint unsigned not null auto_increment,
-    ant_texto varchar(256) not null,
-    ant_data_hora datetime not null,
-    ant_usr_id bigint unsined not null,
-    primary key (ant_id),
-    foreign key ant_usr_fk (ant_usr_id) references usr_usuario(usr_id)
+-- Create the 'uau_usuario_autorizacao' table
+CREATE TABLE uau_usuario_autorizacao (
+    usr_id BIGINT UNSIGNED NOT NULL,
+    aut_id BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (usr_id, aut_id),
+    FOREIGN KEY aut_usuario_fk (usr_id) REFERENCES usr_usuario (usr_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY aut_autorizacao_fk (aut_id) REFERENCES aut_autorizacao (aut_id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-insert into usr_usuario (usr_nome, usr_senha)
-    values ('admin', '$2a$10$i3.Z8Yv1Fw10I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
-insert into aut_autorizacao ()
-    values ('ROLE_ADMIN')
-insert into uau_usuario_autorizacao (usr_id, aut_id)
-    values (1,1);
-insert into ant_anotacao (ant_texto, ant_data_hora, ant_usr_id)
-    values ('My new project', '2023-09-30   19:18', 1);
+-- Create the 'ant_anotacao' table
+CREATE TABLE ant_anotacao (
+    ant_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    ant_texto VARCHAR(256) NOT NULL,
+    ant_data_hora DATETIME NOT NULL,
+    ant_usr_id BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (ant_id),
+    FOREIGN KEY ant_usr_fk (ant_usr_id) REFERENCES usr_usuario(usr_id)
+);
+
+-- Insert data into 'usr_usuario' table
+INSERT INTO usr_usuario (usr_nome, usr_senha)
+VALUES ('admin', '$2a$10$i3.Z8Yv1Fw10I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
+
+-- Insert data into 'aut_autorizacao' table
+INSERT INTO aut_autorizacao (aut_nome)
+VALUES ('ROLE_ADMIN');
+
+-- Insert data into 'uau_usuario_autorizacao' table
+INSERT INTO uau_usuario_autorizacao (usr_id, aut_id)
+VALUES (1, 1);
+
+-- Insert data into 'ant_anotacao' table
+INSERT INTO ant_anotacao (ant_texto, ant_data_hora, ant_usr_id)
+VALUES ('My new project', '2023-09-30 19:18', 1);
